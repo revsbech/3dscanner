2212c451613826c6a074c224544a7b9c64989083
Jan-Erik Revsbech <janerik@moc.net>
2017-03-11 22:28:48 +0100

    Initial Commit

---
+++ Scanner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scanner
+//
+//  Created by Jan-Erik Revsbech on 11/03/17.
+//  Copyright © 2017 Jan-Erik Revsbech. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Scanner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by Jan-Erik Revsbech on 11/03/17.
+//  Copyright © 2017 Jan-Erik Revsbech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
